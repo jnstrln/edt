18eba916eff297f87ccae41de10e8e230344c9d3
stierlij <jean.stierlin@grenoble-inp.org>
2024-10-26 18:03:18 +0200

    maj chemins

--- run_link_generator.sh
+++ run_link_generator.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
 # Exécute le script Python et capture la sortie
-url=$(python3 /home/jeanstierlin/Documents/perso/edt/py_sources/link_generator.py)
+url=$(./py_sources/link_generator.py)
 
 # Ouvre l'URL dans Firefox
 firefox "$url"
+
+# Supprime le dossier __pycache__ s'il existe
+if [ -d "py_sources/__pycache__" ]; then
+    rm -r py_sources/__pycache__
+fi
